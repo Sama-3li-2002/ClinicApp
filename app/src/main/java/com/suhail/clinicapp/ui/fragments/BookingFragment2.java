7a48687d04caffff32551ab55df1ce89e99a939c
suhail-alhassanat <suhail.alhassanat@gmail.com>
2022-08-21 04:00:01 +0300

    enable evening and morning buttons in booking fragment

--- app/src/main/java/com/suhail/clinicapp/ui/fragments/BookingFragment2.java
+++ app/src/main/java/com/suhail/clinicapp/ui/fragments/BookingFragment2.java
@@ -1,5 +1,6 @@
 package com.suhail.clinicapp.ui.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.suhail.clinicapp.R;
 import com.suhail.clinicapp.databinding.FragmentBooking2Binding;
 
 public class BookingFragment2 extends Fragment {
@@ -21,6 +23,28 @@ public class BookingFragment2 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         FragmentBooking2Binding binding=FragmentBooking2Binding.inflate(getLayoutInflater());
+        binding.btnMorning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.btnMorning.setBackgroundResource(R.drawable.shape_btn_clicked);
+                binding.btnMorning.setTextColor(Color.parseColor("#FFFFFF"));
+                binding.btnEvening.setBackgroundResource(R.drawable.shape_btn_unclicked);
+                binding.btnEvening.setTextColor(Color.parseColor("#042D3B"));
+                binding.cvEvening.setVisibility(View.GONE);
+                binding.cvMorning.setVisibility(View.VISIBLE);
+            }
+        });
+        binding.btnEvening.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.btnEvening.setBackgroundResource(R.drawable.shape_btn_clicked);
+                binding.btnEvening.setTextColor(Color.parseColor("#FFFFFF"));
+                binding.btnMorning.setBackgroundResource(R.drawable.shape_btn_unclicked);
+                binding.btnMorning.setTextColor(Color.parseColor("#042D3B"));
+                binding.cvMorning.setVisibility(View.GONE);
+                binding.cvEvening.setVisibility(View.VISIBLE);
+            }
+        });
         return binding.getRoot();
     }
 }
