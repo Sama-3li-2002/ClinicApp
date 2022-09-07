658cfc52f0901d6d3792ed5ac93301521ef7ca86
suhail-alhassanat <suhail.alhassanat@gmail.com>
2022-09-07 16:45:55 +0300

    restore fragment_booking2

--- app/src/main/java/com/suhail/clinicapp/ui/fragments/BookingFragment2.java
+++ app/src/main/java/com/suhail/clinicapp/ui/fragments/BookingFragment2.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.suhail.clinicapp.R;
 import com.suhail.clinicapp.databinding.FragmentBooking2Binding;
+import com.suhail.clinicapp.databinding.TestBinding;
 import com.suhail.clinicapp.listeners.OnClinicClickedListener;
 
 import java.util.Date;
