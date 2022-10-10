5c04b2a1f6d9dbb61cabc4ef3337f82b34cb18bc
Sama-3li-2002 <samaaliallham@gmail.com>
2022-10-10 21:58:39 +0300

    Updatte2

--- app/src/main/java/com/suhail/clinicapp/ui/fragments/AppointmentsDoctorFragment.java
+++ app/src/main/java/com/suhail/clinicapp/ui/fragments/AppointmentsDoctorFragment.java
@@ -32,7 +32,6 @@ public class AppointmentsDoctorFragment extends Fragment {
     public AppointmentsDoctorFragment() {
         // Required empty public constructor
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
